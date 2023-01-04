121051b3c938fa6150190febe8e54b2135027e0b
Sofía Millán <sofiaisabellamillan16@gmail.com>
2023-01-03 22:07:24 -0500

    First commit

---
+++ data/data.js
@@ -0,0 +1,26 @@
+export const products = [
+    {
+        name:"Pan",
+        price:2000,
+        amount:4,
+        category: "Panadería"
+    },
+    {
+        name:"Leche",
+        price:2500,
+        amount:2,
+        category: "Lácteos"
+    },
+    {
+        name:"Salchichón",
+        price:4500,
+        amount:1,
+        category: "Carnes frías"
+    },
+    {
+        name:"Arroz",
+        price:2500,
+        amount:10,
+        category:"Cereales"
+    }
+]
---
+++ scripts/script.js
@@ -0,0 +1,107 @@
+import { products } from "../data/data.js";
+
+let selectedrow = null;
+const tbody = document.getElementById("table-body");
+let name = document.querySelector("#name");
+let price = document.querySelector("#price");
+let amount = document.querySelector("#amount");
+let category = document.querySelector("#category");
+const form = document.querySelector("#formulario");
+let guard = "";
+
+const listar = (lista, contenedor) => {
+  contenedor.innerHTML = "";
+  lista.forEach((product) => {
+    const row = document.createElement("tr");
+    row.classList.add("table-row");
+    row.innerHTML = `
+        <td>${product.name}</td>
+        <td>${product.price}</td>
+        <td>${product.amount}</td>
+        <td>${product.category}</td>
+        <td><button class="delete" name="${product.name}">Eliminar</button>
+            <button class="edit" name="${product.name}">Editar</button></td>
+        `;
+    contenedor.appendChild(row);
+    selectedrow = null;
+  });
+};
+listar(products, tbody);
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const { target } = event;
+ 
+  const valuesForm = Object.values(form);
+  if (selectedrow == null) {
+    console.log(valuesForm);
+    const newProduct = {};
+
+    valuesForm.forEach((valueInput) => {
+      if (valueInput.id) {
+        newProduct[valueInput.id] = valueInput.value;
+        console.log(valueInput);
+      }
+    });
+    products.push(newProduct);
+ 
+    valuesForm.forEach((input) => {
+      if (input.id) {
+        input.value = "";
+      }
+    });
+    listar(products, tbody);
+  } else {
+   
+    products.forEach((p)=>{
+        if(guard==p.name){
+            p.name=name.value;
+            p.price=price.value;
+            p.amount=amount.value;
+            p.category=category.value;
+        }
+    })
+    selectedrow.children[0].textContent = name.value;
+    selectedrow.children[1].textContent = price.value;
+    selectedrow.children[2].textContent = amount.value;
+    selectedrow.children[3].textContent = category.value;
+
+    name.value="";
+    price.value="";
+    category.value="";
+    amount.value="";
+
+    listar(products, tbody);
+  }
+});
+
+document.addEventListener("click", (event) => {
+  const { target } = event;
+
+  if (target.classList.contains("delete")) {
+    const confirmDelete = confirm(`¿Está seguro de eliminar ${target.name}?`);
+    if (confirmDelete) {
+      const productName = target.name;
+      const positionProduct = products.findIndex(
+        (product) => product.name === productName
+      );
+      products.splice(positionProduct, 1);
+      listar(products, tbody);
+    }
+  }
+
+  if (target.classList.contains("edit")) {
+    selectedrow = target.parentElement.parentElement;
+    document.querySelector("#name").value = selectedrow.children[0].textContent;
+    document.querySelector("#price").value =
+      selectedrow.children[1].textContent;
+    document.querySelector("#amount").value =
+      selectedrow.children[2].textContent;
+    document.querySelector("#category").value =
+      selectedrow.children[3].textContent;
+    console.log(target);
+    guard = target.name;
+    console.log(guard);
+
+  }
+});
