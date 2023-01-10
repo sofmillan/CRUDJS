c525d22d54439d10d020f406cf0926fe77c3be02
Sofía Millán <sofiaisabellamillan16@gmail.com>
2023-01-10 09:38:32 -0500

    Second commit, changes CSS

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ let price = document.querySelector("#price");
 let amount = document.querySelector("#amount");
 let category = document.querySelector("#category");
 const form = document.querySelector("#formulario");
-let guard = "";
+let targetName = "";
 
 const listar = (lista, contenedor) => {
   contenedor.innerHTML = "";
@@ -24,6 +24,7 @@ const listar = (lista, contenedor) => {
         `;
     contenedor.appendChild(row);
     selectedrow = null;
+    
   });
 };
 listar(products, tbody);
@@ -33,45 +34,45 @@ form.addEventListener("submit", (event) => {
   const { target } = event;
  
   const valuesForm = Object.values(form);
-  if (selectedrow == null) {
-    console.log(valuesForm);
-    const newProduct = {};
-
-    valuesForm.forEach((valueInput) => {
-      if (valueInput.id) {
-        newProduct[valueInput.id] = valueInput.value;
-        console.log(valueInput);
-      }
-    });
-    products.push(newProduct);
- 
-    valuesForm.forEach((input) => {
-      if (input.id) {
-        input.value = "";
-      }
-    });
-    listar(products, tbody);
-  } else {
-   
-    products.forEach((p)=>{
-        if(guard==p.name){
-            p.name=name.value;
-            p.price=price.value;
-            p.amount=amount.value;
-            p.category=category.value;
+  
+    if (selectedrow == null) {
+     // console.log(valuesForm);
+      const newProduct = {};
+  
+      valuesForm.forEach((valueInput) => {
+        if (valueInput.id) {
+          newProduct[valueInput.id] = valueInput.value;
+       //   console.log(valueInput);
         }
-    })
-    selectedrow.children[0].textContent = name.value;
-    selectedrow.children[1].textContent = price.value;
-    selectedrow.children[2].textContent = amount.value;
-    selectedrow.children[3].textContent = category.value;
-
-    name.value="";
-    price.value="";
-    category.value="";
-    amount.value="";
-
-    listar(products, tbody);
+      });
+      products.push(newProduct);
+   
+      valuesForm.forEach((input) => {
+        if (input.id) {
+          input.value = "";
+        }
+      });
+      console.log(products)
+      listar(products, tbody);
+    } else {
+     
+      products.forEach((p)=>{
+          if(targetName==p.name){
+              p.name=name.value;
+              p.price=price.value;
+              p.amount=amount.value;
+              p.category=category.value;
+          }
+      })
+      // selectedrow.children[0].textContent = name.value;
+  
+      name.value="";
+      price.value="";
+      category.value="";
+      amount.value="";
+      
+      listar(products, tbody);
+  console.log(products)
   }
 });
 
@@ -86,6 +87,8 @@ document.addEventListener("click", (event) => {
         (product) => product.name === productName
       );
       products.splice(positionProduct, 1);
+      console.log(products)
+
       listar(products, tbody);
     }
   }
@@ -99,9 +102,9 @@ document.addEventListener("click", (event) => {
       selectedrow.children[2].textContent;
     document.querySelector("#category").value =
       selectedrow.children[3].textContent;
-    console.log(target);
-    guard = target.name;
-    console.log(guard);
+   // console.log(target);
+    targetName = target.name;
+  //  console.log(guard);
 
   }
 });
